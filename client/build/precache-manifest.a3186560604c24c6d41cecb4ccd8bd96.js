4f148622b4db232f07285bd8ccdcd111c311cd5d
Sajees XPS <sajees.jeya@gmail.com>
2022-12-24 02:14:30 -0500

    still to fix build

---
+++ client/build/precache-manifest.a3186560604c24c6d41cecb4ccd8bd96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1810dd16e0bc5ee9225a4b62118b892f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ccf0cdfb23f1557cf8",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "1ee3f44f580331c161cb",
+    "url": "/static/js/2.2b08a43e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.2b08a43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ccf0cdfb23f1557cf8",
+    "url": "/static/js/main.6ccc6f3f.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
