99c68b843facf3ce0aa0d4d31ac5204bad5490fc
Sajees XPS <sajees.jeya@gmail.com>
2022-12-27 03:55:28 -0500

    added stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js'; 
+import { useLazyQuery } from '@apollo/client'; 
+import { QUERY_CHECKOUT } from '../../utils/queries';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth'; 
 import { useStoreContext } from '../../utils/GlobalState';
@@ -6,9 +9,20 @@ import { TOGGLE_CART, AAD_MULTIPLE_TO_CART, ADD_MULTIPLE_TO_CART } from '../../u
 import { idbPromise } from "../../utils/helpers"
 import './style.css';
 
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
 
     useEffect(() => {
         async function getCart() {
@@ -32,6 +46,20 @@ const Cart = () => {
         });
 
         return sum.toFixed(2);
+    }
+
+    function submitCheckout() {
+        const productsIds = []; 
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productsIds.push(item._id);
+            }
+        }); 
+
+        getCheckout({
+            variables: { products: productIds }
+        }); 
     }
     
     if (!state.cartOpen) {
@@ -57,7 +85,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ? 
-                              <button>
+                              <button onClick={submitCheckout}>
                                 Checkout
                               </button>
                             :
