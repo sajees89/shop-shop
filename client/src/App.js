62c106c8f4a872c5909693211c01c73758d7dbcd
Sajees XPS <sajees.jeya@gmail.com>
2022-12-27 18:08:55 -0500

    updated packages

--- client/src/App.js
+++ client/src/App.js
@@ -47,6 +47,7 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
