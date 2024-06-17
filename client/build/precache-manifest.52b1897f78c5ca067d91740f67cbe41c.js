2ce1340459b328ff0b047f432097bcf1f06a9861
Sajees XPS <sajees.jeya@gmail.com>
2024-06-17 18:11:35 -0400

    react script

---
+++ client/build/precache-manifest.52b1897f78c5ca067d91740f67cbe41c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62eb891e4b5321ff966cb5bee6f8ee25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf0e1ede6ca9920a5cc",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "921e6749c810354c8536",
+    "url": "/static/js/2.ce14c807.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.ce14c807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf0e1ede6ca9920a5cc",
+    "url": "/static/js/main.3b94da8d.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
